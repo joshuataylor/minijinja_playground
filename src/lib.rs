dac076f7b9b6301c817dc69de9c49e3a7da2849e
Josh Taylor <joshuataylorx@gmail.com>
2023-01-10 19:17:29 +0800

    nicer rendering

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,38 @@ use chrono::Utc;
 use minijinja::machinery::CompiledTemplate;
 use minijinja::value::Value;
 use minijinja::{render, Environment};
+
+use std::{fmt, panic};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{console, HtmlTextAreaElement};
-use web_sys::{Document, Element, HtmlButtonElement, Window};
+use web_sys::HtmlButtonElement;
+use web_sys::HtmlTextAreaElement;
+
+enum ParseErrorKind {
+    Variable,
+    MiniJinja,
+}
+
+impl fmt::Display for ParseErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ParseErrorKind::Variable => write!(f, "variables"),
+            ParseErrorKind::MiniJinja => write!(f, "minijinja"),
+        }
+    }
+}
+
+struct ParseError {
+    kind: ParseErrorKind,
+    message: String,
+}
+
+static MINIJINJA_TEMPLATE: &str = include_str!("../rendered.html.jinja");
 
 #[wasm_bindgen(start)]
 pub fn run() -> Result<(), JsValue> {
-    console_error_panic_hook::set_once();
+    // console_error_panic_hook::set_once();
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     let window = web_sys::window().expect("should have a window in this context");
     let document = window.document().expect("window should have a document");
@@ -26,12 +50,6 @@ pub fn run() -> Result<(), JsValue> {
         .set_attribute("style", "display: block")
         .unwrap_throw();
 
-    setup_minijinja_form(&window, &document)?;
-
-    Ok(())
-}
-
-fn setup_minijinja_form(_window: &Window, document: &Document) -> Result<(), JsValue> {
     let rendered_element = document
         .get_element_by_id("render-wrapper")
         .expect("should have #render-wrapper on the page");
@@ -44,8 +62,42 @@ fn setup_minijinja_form(_window: &Window, document: &Document) -> Result<(), JsV
         .get_element_by_id("variables")
         .expect("should have #variables on the page");
 
+    let template_string = match render_template(
+        &template_element
+            .dyn_ref::<HtmlTextAreaElement>()
+            .expect("#template be an `HtmlTextAreaElement`")
+            .value(),
+        &variables_element
+            .dyn_ref::<HtmlTextAreaElement>()
+            .expect("#template be an `HtmlTextAreaElement`")
+            .value(),
+    ) {
+        Ok(rendered) => rendered,
+        Err(e) => {
+            render!(MINIJINJA_TEMPLATE, error_message => e.message, error_kind => e.kind.to_string())
+        }
+    };
+
+    rendered_element.set_inner_html(&template_string);
+
     let update_preview_closure = Closure::<dyn Fn()>::new(move || {
-        update_preview(&rendered_element, &template_element, &variables_element)
+        let template_string = match render_template(
+            &template_element
+                .dyn_ref::<HtmlTextAreaElement>()
+                .expect("#template be an `HtmlTextAreaElement`")
+                .value(),
+            &variables_element
+                .dyn_ref::<HtmlTextAreaElement>()
+                .expect("#template be an `HtmlTextAreaElement`")
+                .value(),
+        ) {
+            Ok(rendered) => rendered,
+            Err(e) => {
+                render!(MINIJINJA_TEMPLATE, error_message => e.message, error_kind => e.kind.to_string())
+            }
+        };
+
+        rendered_element.set_inner_html(&template_string);
     });
 
     document
@@ -60,34 +112,18 @@ fn setup_minijinja_form(_window: &Window, document: &Document) -> Result<(), JsV
     Ok(())
 }
 
-fn update_preview(template_element: &Element, template: &Element, variables: &Element) {
-    let template_textarea = template
-        .dyn_ref::<HtmlTextAreaElement>()
-        .expect("#template be an `HtmlTextAreaElement`");
-
-    let variables_textarea = variables
-        .dyn_ref::<HtmlTextAreaElement>()
-        .expect("#variables_textarea be an `HtmlTextAreaElement`");
-
-    let variables_value = variables_textarea.value();
-
-    // The variables here may or not be valid JSON, we don't care unless the user has input later.
-    // @todo only check when the user has input
-    // @todo check that the variables are valid JSON before this point?
-    let parsed_variables = serde_json_wasm::from_str(&variables_value);
-
-    if !variables_value.is_empty() && parsed_variables.is_err() {
-        template_element.set_inner_html("Variables is not a valid JSON object.");
-        return;
+fn validate_variables(variables: &str) -> Result<Value, ParseError> {
+    if variables.is_empty() {
+        // @todo this is dumb, just return an empty object?
+        return Ok(Value::default());
     }
 
-    let ctx: Value = if variables_value.is_empty() {
-        serde_json_wasm::from_str("{}").unwrap()
-    } else {
-        parsed_variables.unwrap_throw()
-    };
+    // The variables here may or not be valid JSON, we don't care unless the user has input later.
+    serde_json_wasm::from_str(variables).map_err(wasm_error_to_string)
+}
 
-    let templates_value = template_textarea.value();
+fn render_template(template_string: &str, variables_string: &str) -> Result<String, ParseError> {
+    let variables = validate_variables(variables_string)?;
 
     // Here we define the environment each time.
     // Might be better to define it once, but given it's quick to do this,
@@ -97,48 +133,61 @@ fn update_preview(template_element: &Element, template: &Element, variables: &El
 
     // Later we'll support multiple templates, so let's just use this now, instead
     // of direct rendering via the `render!` macro
-    env.add_template("playground", &templates_value)
-        .unwrap_throw();
+    env.add_template("playground", template_string)
+        .map_err(minijinja_error_to_string)?;
 
-    let template = env.get_template("playground").unwrap_throw();
+    let template = env
+        .get_template("playground")
+        .map_err(minijinja_error_to_string)?;
 
     // @todo use timestamp_nanos later
     let start: i64 = Utc::now().timestamp_micros();
-    let rendered = template.render(ctx).unwrap_throw();
+    let rendered = template
+        .render(variables)
+        .map_err(minijinja_error_to_string)?;
     let diff = Utc::now().timestamp_micros() - start;
 
-    // @#todo: I will fix this double compile later. Forgive me for my sins.
+    // Can we do this without recompiling the template?
     let compiled =
-        CompiledTemplate::from_name_and_source("playground", &templates_value).unwrap_throw();
-    let compiled_instructions = minijinja::machinery::instructions_list(compiled.instructions);
+        CompiledTemplate::from_name_and_source("playground", template_string).unwrap_throw();
 
-    let compiled_template =
-        CompiledTemplate::from_name_and_source("playground", &templates_value).unwrap_throw();
-
-    // List containing HTML elements of instructions
     let mut instructions_list: String = String::new();
 
     let mut last_line: Option<usize> = None;
-    for (idx, instr) in compiled_instructions.iter().enumerate() {
-        let line = compiled_template.instructions.get_line(idx);
-        let line_info = if line == last_line {
-            format!("{}", line.unwrap())
+    for i in 0..compiled.instructions.len() {
+        let instruction = compiled.instructions.get(i).unwrap_throw();
+        let instruction_line = compiled.instructions.get_line(i);
+        let line_info = if Some(instruction_line) == Some(last_line) {
+            format!("{}", instruction_line.unwrap_or(0))
         } else {
             "-".to_string()
         };
 
-        let instruction_info = format!("{idx:>05x}");
-        let instruction_info2 = format!("{instr:?}");
+        let id = format!("{i:>05x}");
+        let instruction_output = format!("{instruction:?}");
 
-        instructions_list.push_str(&format!("<tr><td class='whitespace-nowrap'>{line_info}</td><td class='whitespace-nowrap'>{instruction_info}</td><td class='whitespace-nowrap'>{instruction_info2}</td></tr>"));
+        instructions_list.push_str(&format!("<tr><td class='whitespace-nowrap'>{line_info}</td><td class='whitespace-nowrap'>{id}</td><td class='whitespace-nowrap'>{instruction_output}</td></tr>"));
 
-        last_line = line;
+        last_line = instruction_line;
     }
 
-    // fuel info
     let fuel_info = env.fuel().unwrap_or(0);
-    let mj_template = include_str!("../rendered.html.jinja");
 
-    let output = render!(mj_template, rendered => rendered, errors => "", instructions => instructions_list, fuel_cost => fuel_info.to_string(), render_time => format!("{diff}ms"));
-    template_element.set_inner_html(&output);
+    Ok(
+        render!(MINIJINJA_TEMPLATE, rendered => rendered, errors => "", instructions => instructions_list, fuel_cost => fuel_info.to_string(), render_time => format!("{diff}ms")),
+    )
+}
+
+fn minijinja_error_to_string(error: minijinja::Error) -> ParseError {
+    ParseError {
+        kind: ParseErrorKind::MiniJinja,
+        message: format!("{error:#?}"),
+    }
+}
+
+fn wasm_error_to_string(error: serde_json_wasm::de::Error) -> ParseError {
+    ParseError {
+        kind: ParseErrorKind::Variable,
+        message: format!("{error:#?} - {error}"),
+    }
 }
